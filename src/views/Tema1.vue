629625a01712e2107d05b9e407c092b50253e1e9
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2022-05-09 22:35:17 -0500

    Avance 3

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 1
@@ -14,8 +14,6 @@
       .col-md-8.col-12
         p Los sistemas de gestión, planes de continuidad del negocio son de vital importancia; sin embargo, los aspectos técnicos son la base sostenible de la ciberseguridad y de la seguridad informática. Los ataques informáticos lamentablemente pueden ser llevados a cabo por personas sin gran experiencia, quienes por medio de herramientas básicas pueden conseguir objetivos de fácil acceso. Es importante resaltar que tener conocimiento del modus operandi de la ciberdelincuencia, articulado a conocimientos técnicos marcan la diferencia en cargos relacionados con la ciberseguridad y la seguridad informática, ya que de esta manera se puede evidenciar un ataque difícil de ser detectado. 
         p Por lo anterior, la inversión en capacitación de personal es indiscutiblemente el pilar fundamental de toda implementación de seguridad, teniendo en cuenta que adquirir sistemas de seguridad con altas capacidades y características lleva a las organizaciones a entrar en un estado de falsa seguridad, dando un segundo lugar a las capacidades humanas.
-      
-    separador
 
     #t_1_1.titulo-segundo.color-acento-contenido(data-aos="fade-right")
       h2 1.1	Conceptos y generalidades (triada de la seguridad)
@@ -54,9 +52,10 @@
           span Conceptos importantes en el <em>hacking</em> ético
         figure(data-aos="fade-right")
           img(src='@/assets/curso/tema1/img04.svg')  
-    
-    .row.justify-content-center.BGIMG03
-      .col-md-9.col-10
+
+  .container.BGIMG03.px-4.px-md-5.mb-5
+    .row.justify-content-center
+      .col-md-9.col-10.mb-4
         figure(data-aos="fade-right")
           img(src='@/assets/curso/tema1/img06.svg')  
       .col-md-12.mt-5.mb-4
@@ -80,7 +79,7 @@
             h5 DISPONIBILIDAD
             p Los usuarios autorizados deben tener acceso a la información cuando requieran.
     
-    .row.justify-content-center.mb-5
+    .row.justify-content-center
       .col-md-9.col-11
         .row.BGIMG04
           .col-2
@@ -96,10 +95,209 @@
           .col-2
             figure(data-aos="fade-left")
               img.img02(src='@/assets/curso/tema1/img10.png') 
-      .col-1
-    .row.justify-content-center
-      
+  
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+    
+    SlyderF.mb-5(columnas="col-12")
+      .tarjeta
+        .row.align-items-center
+          .col-md-6
+            figure.img03
+              img(src='@/assets/curso/tema1/img11.png')
+          .col-md-6.BG03.p-4
+            p La confidencialidad puede violarse de muchas maneras, como ataques directos destinados a obtener acceso no autorizado a sistemas, aplicaciones y bases de datos, con el fin de robar o manipular datos, el reconocimiento de redes y otros tipos de escaneos, las escuchas electrónicas (mediante un ataque de hombre en medio) y la escalada de los privilegios del sistema por parte de un atacante son solo algunos ejemplos, pero la confidencialidad también puede ser violada sin querer, por error humano, descuido o controles de seguridad inadecuados, entre los ejemplos cabe citar la incapacidad (por parte de los usuarios o de la seguridad de la tecnología de la información) de proteger adecuadamente las contraseñas; el uso compartido de cuentas de usuario; la escucha física (también conocida como “#[i shoulder surfing]"); la no codificación de los datos (en proceso, en tránsito y cuando están almacenados); los sistemas de autenticación deficientes, débiles o inexistentes; y el robo de equipo físico y dispositivos de almacenamiento.  
+      .tarjeta
+        .row.align-items-center
+          .col-md-6
+            figure.img03
+              img(src='@/assets/curso/tema1/img12.png')
+          .col-md-6.BG03.p-4
+            p Integridad: está inmersa en poder dar a los datos características como precisión, confiabilidad y consistencia, estos a su vez no deben ser alterados en su paso y se toman medidas de seguridad que garanticen que no se deben modificar, se realiza a través de permisos y controles de cada uno de los usuarios autorizados al acceso.
+            p En el uso diario, la integridad se refiere a la calidad de algo que está entero o completo, se refiere a asegurar que los datos no han sido manipulados y, por lo tanto, se puede confiar en ellos, es correcto, auténtico y fiable, como ocurre con la confidencialidad, la integridad puede verse comprometida directamente a través de un vector de ataque (como la manipulación de los sistemas de detección de intrusos, la modificación de los archivos de configuración o el cambio de los registros del sistema para eludir la detección) o involuntariamente, por error humano, falta de cuidado, errores de codificación o políticas, procedimientos y mecanismos de protección inadecuados.
+      .tarjeta
+        .row.align-items-center
+          .col-md-6
+            figure.img03
+              img(src='@/assets/curso/tema1/img13.png')
+          .col-md-6.BG03.p-md-5.p-4
+            p Disponibilidad: radica en que la infraestructura de la información debe estar controlada rigurosamente, manteniendo el hardware en debido funcionamiento sin errores, para que de esta manera siempre que se necesite el acceso a la información esté disponible, evitando cuellos de botella y otros factores que pongan en riesgo la información.
+            p Es por ello que los sistemas, las aplicaciones y los datos tienen poco valor para una organización y sus clientes si no son accesibles cuando los usuarios autorizados los necesitan, es por ello que la disponibilidad significa que las redes, los sistemas y las aplicaciones están en funcionamiento, garantizando que los usuarios autorizados tengan acceso oportuno y fiable a los recursos cuando los necesiten.
 
+    .row.BGIMG05.justify-content-center.mb-4
+      .col-md-8.col-10.mt-5
+        p.mt-5 Muchas cosas pueden poner en peligro la disponibilidad, entre ellas las fallas de hardware o software, los cortes de energía, los desastres naturales y los errores humanos, tal vez el ataque más conocido que amenaza la disponibilidad sea el de negación del servicio, en el que el rendimiento de un sistema, un sitio web, una aplicación o un servicio basado en la web se degrada de forma intencionada y maliciosa, o el sistema se vuelve completamente inalcanzable. En la siguiente figura se pueden observar las estadísticas de la ciberseguridad en Colombia.
+      .col-2.mt-5
+        figure.mt-5(data-aos="fade-left")
+          img(src='@/assets/curso/tema1/img15.svg')
+      .col-md-10
+        .titulo-sexto.color-acento-contenido.mb-5(data-aos="fade-right")
+          h5 Figura 3
+          span Estadísticas de la ciberseguridad en Colombia
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema1/img16.svg')
+
+    #t_1_2.titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 1.2	1.2	Fases de #[i hacking]
+    
+    .row.mb-4
+      .col-md-7
+        p La seguridad de la información es una creciente preocupación para las personas y las empresas, la protección de sus datos se ha convertido en una premisa, ya que los ataques por parte de ciberdelincuentes cada vez son más comunes; sin embargo, los riesgos y constantes amenazas dejan al desnudo los sistemas de información y los peligros que en la era digital se están afrontando en el mundo de hoy, es por ello que se ha hecho fundamental que tenga conocimientos informáticos y de seguridad para poder aplicarlos en los sistemas y redes, para poder solventar fallos a tiempo, documentarlos y atacarlos de forma efectiva.
+        .cajon.color-acento-botones.p-4(data-aos="fade-right")
+          p Para ello, se usan pruebas de penetración o conocidas comúnmente como pentesting, las cuales se usan para poder detectar riesgos en sus sistemas de información o redes, su uso pretende encontrar fallas en la seguridad de la información, saltándose las medidas de seguridad adoptadas por un individuo, empresa u organización, existen principalmente 5 fases del hacking, un hacker no necesariamente tiene seguirlas de forma secuencial; pero cuando se lleva de forma secuencial se obtienen mejores resultados.
+      .col-md-5.mb-4
+        figure(data-aos="fade-left")
+          img(src='@/assets/curso/tema1/img17.png')
+      .col-md-4.col-6
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema1/img18.svg')
+    
+    .row.justify-content-center.mb-4
+      .col-md-10
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema1/img19.svg')
+
+    .row.BGIMG06.justify-content-center.p-4.mb-4
+      .col-md-7
+        p En esta fase de reconocimiento la huella proporciona información importante como:
+        .row.align-items-center.mb-4
+          .col-1
+          .col-6
+            ul.lista-ul
+              li 
+                i.fas.fa-dot-circle
+                | El nombre de dominio.
+              li 
+                i.fas.fa-dot-circle
+                | Los servicios TCP y UDP.
+              li 
+                i.fas.fa-dot-circle
+                | Los nombres de los sistemas y las contraseñas.
+          .col-3
+            figure(data-aos="fade-right")
+              img(src='@/assets/curso/tema1/img22.png')
+        p Igualmente, hay otras formas de realizar la huella, como la suplantación de la identidad de un sitio web mediante su duplicación, el uso de motores de búsqueda para encontrar información sobre la organización e incluso el uso de la información de los empleados actuales para la suplantación.
+        p En la siguiente tabla se presenta un comparativo entre la recolección activa y la recolección pasiva.
+      .col-md-5
+        figure(data-aos="fade-left")
+          img(src='@/assets/curso/tema1/img20.png')
+
+    .titulo-sexto.color-acento-contenido(data-aos="fade-right")
+      h5 Tabla 1
+      span Tipos de reconocimiento
+    
+    .tabla-b.color-acento-contenido.mb-5 
+      table
+        thead
+          tr
+            th(style="background-color: #E01650; color: #fff").text-center Recolección activa
+            th(style="background-color: #E01650; color: #fff").text-center Recolección pasiva
+        tbody
+          tr
+            td(style="background-color: #F6F6F6; color: #000")
+              p.text-bold Es de gran efectividad, pero no es la mejor aliada si de anonimato se quiere hablar, porque puede llamar mucho la atención del objetivo:
+              ul.lista-ul
+                li 
+                  i.fas.fa-angle-right
+                  | Campañas de ingeniería social (llamadas falsas).
+                li 
+                  i.fas.fa-angle-right
+                  | Metasploitable.
+                li 
+                  i.fas.fa-angle-right
+                  | Ataques al objetivo u escaneos invasivos.
+            td(style="background-color: #F6F6F6; color: #000")
+              p.text-bold Es cuando se tiene un contacto directo con el objetivo, es decir, cuando existe el apoyo de fuentes públicas para obtener información, lo cual ayuda a mantener el anonimato y es menos “ruidosa” que la activa; estas fuentes pueden ser:
+              ul.lista-ul
+                li 
+                  i.fas.fa-angle-right
+                  | Sitios web relacionados con el objetivo.
+                li 
+                  i.fas.fa-angle-right
+                  | Motores de búsqueda.
+                li 
+                  i.fas.fa-angle-right
+                  | Redes sociales.
+                li 
+                  i.fas.fa-angle-right
+                  | Blogs.
+    .row.mb-4
+      .col-md-3.col-5
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema1/img23.svg')   
+    
+    .row.justify-content-center.mb-4
+      .col-md-8
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema1/img24.svg') 
+
+    p En la siguiente tabla puede observar algunas características de cada método.
+
+    .titulo-sexto.color-acento-contenido(data-aos="fade-right")
+      h5 Tabla 2
+      span Métodos de escaneo
+    
+    .tabla-b.color-acento-contenido.mb-5 
+      table
+        thead
+          tr
+            th(style="background-color: #E01650; color: #fff").text-center Fase de pre-ataque
+            th(style="background-color: #E01650; color: #fff").text-center Rastreo de puertos
+            th(style="background-color: #E01650; color: #fff").text-center Extraer información
+        tbody
+          tr
+            td(style="background-color: #F6F6F6; color: #000") Escaneo se refiere a la fase de pre-ataque cuando el atacante busca en la red para obtener información específica sobre la base de la información recopilada durante el reconocimiento.
+            td(style="background-color: #F6F6F6; color: #000") La exploración puede incluir el uso de marcadores, analizadores de puertos, la cartografía de la red, barrido, escáneres de vulnerabilidades, entre otros.
+            td(style="background-color: #F6F6F6; color: #000") Los atacantes extraen información como los nombres de equipos, direcciones IP y cuentas de usuario para lanzar ataques posteriores.
+
+    .row.mb-4
+      .col-md-4.col-5
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema1/img25.svg')   
+    
+    .row.justify-content-center.mb-4
+      .col-md-10.mb-4
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema1/img26.svg')
+      .col-md-10.mb-4
+        .row.BGIMG07.align-items-center.p-2
+          .col-10
+            p Los accesos hacen referencia al punto donde el atacante obtiene acceso al sistema operativo, a aplicaciones en el computador, al dispositivo o a la red, dentro de ella se puede realizar el descifrado de contraseñas, los desbordamientos de buffer, la denegación de servicio, el secuestro de sesión, entre los más comunes.
+          .col-2
+            figure(data-aos="fade-left")
+              img(src='@/assets/curso/tema1/img28.png')
+
+    .row.mb-4
+      .col-md-5.col-6
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema1/img29.svg')   
+    
+    .row.justify-content-end.mb-5
+      .col-md-9
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema1/img30.svg')
+
+    .row.justify-content-center.mb-4
+      .col-md-10.BGIMG08.p-5
+        .row.justify-content-end.py-5
+          .col-7
+            p Mantener el acceso es el fin de esta fase, lo que se trata de poder permanecer con el control del sistema, los atacantes pueden impedir que el sistema sea propiedad de otros atacantes, asegurando su acceso exclusivo con puertas traseras, rootkits y troyanos e incluso usan ya el sistema que se encuentra comprometido para poder lanzar nuevos ataques.
+
+
+    .row.mb-4
+      .col-md-4.col-5
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema1/img32.svg')   
+    
+    .row.justify-content-center.mb-5
+      .col-md-10
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema1/img33.svg') 
+    
+    .row.justify-content-center.mb-4
+      .col-md-10.BGIMG09.p-5
+        .row.py-5
+          .col-7
+            p Las intenciones del atacante incluyen acceso continuo a los sistemas víctimas, pasar inadvertido y eliminar las pruebas que puedan conducir a su identificación, es por ello que se disfraza entre el sistema para no ser detectado y así evitar cualquier sospecha, haciendo pensar que es parte del sistema.
 
 
 </template>
@@ -108,9 +306,7 @@
 export default {
   name: 'Tema1',
   components: {},
-  data: () => ({
-    // variables de vue
-  }),
+  data: () => ({}),
   mounted() {
     this.$nextTick(() => {
       this.$aosRefresh()
